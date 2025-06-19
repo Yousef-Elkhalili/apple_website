49fc6cc6d0962fbe28507fcea5674c47e0a54203
Yousef-Elkhalili <elkhaliliyousef@gmail.com>
2025-06-19 22:15:55 +0300

    Created all needed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Navbar } from "./components/Navbar"
 import { Hero } from "./components/Hero"
 import { Highlights } from "./components/Highlights"
+import { Model } from "./components/Model"
+import { Features } from "./components/Features"
+import { Chip } from "./components/Chip"
+import { Footer } from "./components/Footer"
 
 const App = () => {
 
@@ -9,6 +13,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <Chip />
+      <Footer />
     </main>
   )
 }
